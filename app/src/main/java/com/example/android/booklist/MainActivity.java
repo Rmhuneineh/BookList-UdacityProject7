6a0173e59016df8ca463650fcde6ab848168f282
Ragheed Ma'rouf Huneineh <ragheedmhuneineh@outlook.com>
2017-04-29 16:37:07 +0200

    Fix functionality

--- app/src/main/java/com/example/android/booklist/MainActivity.java
+++ app/src/main/java/com/example/android/booklist/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.android.booklist;
 
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.AsyncTaskLoader;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -14,10 +14,16 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements LoaderCallbacks<List<Book>> {
 
     private static final String GOOGLE_REQUEST_URL = "https://www.googleapis.com/books/v1/volumes?q=";
+
+    private static final int BOOK_LOADER_ID = 1;
+
+    BookAdapter mAdapter;
 
 
     @Override
@@ -35,16 +41,18 @@ public class MainActivity extends AppCompatActivity {
         if(isConnected){
             TextView state = (TextView) findViewById(R.id.internet_text_view);
             state.setVisibility(View.GONE);
-            final BookAsyncTask task = new BookAsyncTask();
+
+            ListView booksListView = (ListView) findViewById(R.id.list_view);
+
+            mAdapter = new BookAdapter(this, new ArrayList<Book>());
+
+            booksListView.setAdapter(mAdapter);
 
             ImageView search = (ImageView) findViewById(R.id.search);
             search.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    EditText bookName = (EditText) findViewById(R.id.book_name);
-                    String url = GOOGLE_REQUEST_URL + bookName.getText().toString();
-                    Log.v("MainActivity", "URL: " + url);
-                    task.execute(url);
+                    updateInfo();
                 }
             });
         } else {
@@ -53,34 +61,68 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void updateUi(ArrayList<Book> books){
-        ListView listView = (ListView) findViewById(R.id.list_view);
-
-        BookAdapter mAdapter = new BookAdapter(this, books);
-
-        listView.setAdapter(mAdapter);
+    private void updateInfo(){
+        android.app.LoaderManager loaderManager = getLoaderManager();
+        loaderManager.initLoader(BOOK_LOADER_ID, null, MainActivity.this);
     }
 
-    public class BookAsyncTask extends AsyncTask<String, Book, ArrayList<Book>>{
+    public static class BookLoader extends AsyncTaskLoader<List<Book>> {
+
+        /** Query URL */
+        private String mUrl;
+
+        /**
+         * Constructs a new {@link BookLoader}.
+         *
+         * @param context of the activity
+         * @param url to load data from
+         */
+        public BookLoader(Context context, String url) {
+            super(context);
+            mUrl = url;
+        }
 
         @Override
-        protected ArrayList<Book> doInBackground(String... urls) {
-            if (urls.length < 1 || urls[0] == null) {
+        protected void onStartLoading() {
+            forceLoad();
+        }
+
+        /**
+         * This is on a background thread.
+         */
+        @Override
+        public List<Book> loadInBackground() {
+            if (mUrl == null) {
                 return null;
             }
 
-
-            ArrayList<Book> result = Utils.fetchBookData(urls[0]);
-            return result;
-        }
-
-        protected void onPostExecute(ArrayList<Book> result) {
-            // If there is no result, do nothing.
-            if (result == null) {
-                return;
-            }
-
-            updateUi(result);
+            List<Book> books = Utils.fetchBookData(mUrl);
+            return books;
         }
     }
+
+    @Override
+    public android.content.Loader<List<Book>> onCreateLoader(int id, Bundle args) {
+        EditText bookName = (EditText) findViewById(R.id.book_name);
+        String title = bookName.getText().toString();
+        title = title.replace(" ", "+");
+        String uriString = GOOGLE_REQUEST_URL + title;
+
+        return new BookLoader(this, uriString);
+    }
+
+    @Override
+    public void onLoadFinished(android.content.Loader<List<Book>> loader, List<Book> books) {
+        mAdapter.clear();
+        if (books != null && !books.isEmpty()) {
+            mAdapter.addAll(books);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(android.content.Loader<List<Book>> loader) {
+        mAdapter.clear();
+    }
+
+
 }
